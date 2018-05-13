7cd1d349b72523de15e95e083dfeed814317887d
Eva Barrett <Evabarrett23@Gmail.com>
2018-05-13 01:43:18 -0700

    fixed game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,28 @@
-var cards = ["queen", "queen", "king", "king"];
+// var cards = ["queen", "queen", "king", "king"];
+
+var cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: "images/queen-of-hearts.png"
+},
+{
+  rank: 'queen',
+  suit: 'diamonds',
+  cardImage: 	"images/queen-of-diamonds.png"
+},
+{
+  rank: 'king',
+  suit: 'hearts',
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: 'king',
+  suit: 'diamonds',
+  cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 var checkForMatch = function(){
   if (cardsInPlay.length === 2) {
@@ -10,9 +34,12 @@ var checkForMatch = function(){
   }
 }
 var flipCard = function(cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
-  console.log(cardsInPlay);
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log("cardsInPlay", cardsInPlay);
+  console.log("ccardImage", cards[cardId].cardImage);
+  console.log("suit", cards[cardId].suit);
+
   checkForMatch();
   //
   // var cardOne = cards[0];
